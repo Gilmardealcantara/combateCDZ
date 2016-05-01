32c04faf9ac3adca33209f400e81d09b869a1061
lauradebella <lauradebella@gmail.com>
2016-05-01 20:10:16 -0300

    Adds js to login and sign up buttons.

---
+++ src/main/webapp/js/header.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.getElementById('b1').addEventListener('click', function(){
+   window.location.pathname = "/combateCDZ/login.xhtml";  
+});
+
+document.getElementById('b2').addEventListener('click', function(){
+   window.location.pathname = "/combateCDZ/gerenciarUsuario.xhtml";  
+});
+
