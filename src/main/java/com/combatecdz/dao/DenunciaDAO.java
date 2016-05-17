55513fb70ef370f53445940b3968f9ddb48c0ac9
Aluno <Aluno@DESKTOP-PALV0JQ>
2016-05-17 08:26:24 -0300

    Add DenunciaDAO Class.

---
+++ src/main/java/com/combatecdz/dao/DenunciaDAO.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.combatecdz.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.Persistence;
+import com.combatecdz.model.Denuncia;
+import java.util.List;
+
+public class DenunciaDAO {
+
+    private EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistencia");
+    private EntityManager em = factory.createEntityManager();
+
+    public boolean inserirDenuncia(Denuncia denuncia) {
+        try {
+            em.getTransaction().begin();
+            em.persist(denuncia);
+            
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deletarDenuncia(int Id) {
+        try {
+            em.getTransaction().begin();
+            Denuncia d = em.find(Denuncia.class, Id);
+            em.remove(d);
+            em.getTransaction().commit();
+            System.out.println("teste2");
+            return true;
+        } catch (Exception e) {
+            System.out.println("teste1");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Denuncia> getTodosUsuarios() {        
+        return em.createQuery("from Denuncia d", Denuncia.class).getResultList();        
+    }
+    
+    
+}
