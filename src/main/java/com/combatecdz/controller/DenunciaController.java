97fd9ffd120a54f4953c16619c3349c8b63ca2b7
Aluno <Aluno@172.16.127.204>
2016-05-30 12:00:58 -0300

    Pegar data sistema. validar dados insert denuncia. likes, sem restrição por usuário.

--- src/main/java/com/combatecdz/controller/DenunciaController.java
+++ src/main/java/com/combatecdz/controller/DenunciaController.java
@@ -8,6 +8,10 @@ package com.combatecdz.controller;
 import com.combatecdz.dao.DenunciaDAO;
 import com.combatecdz.model.Denuncia;
 import com.combatecdz.model.Usuario;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -65,13 +69,37 @@ public class DenunciaController {
     }
 
     public String salvarDenuncia() {
+        
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	Date today = Calendar.getInstance().getTime(); 
+	 
+      
         denuncia.setStatus_den("Pendente");
         
         denuncia.setId_cid(this.getUsuario().getId());
-        
-        DenunciaDAO u = new DenunciaDAO();
-        u.inserirDenuncia(this.getDenuncia());
-        return "cidadao";
+        String data = dateFormat.format(today);
+        denuncia.setData_den(data);
+        int flag = 0;
+        if (denuncia.getEstado().isEmpty() || denuncia.getCidade().isEmpty() || denuncia.getRua().isEmpty()
+                || denuncia.getCep().isEmpty() || denuncia.getDescricao().isEmpty()) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Todos os campos sāo obrigatórios", "Erro no Cadastro!"));
+            return null;
+        }
+        if (denuncia.getEstado().length() != 2) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Estado Inválido", "Erro no Cadastro!"));
+                flag = 1;
+            }
+        if (denuncia.getCep().length() != 8) {
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "CEP Inválido", "Erro no Cadastro!"));
+                flag = 1;
+        }
+        if (flag == 1) {
+                return null;
+        } else {
+            DenunciaDAO u = new DenunciaDAO();
+            u.inserirDenuncia(this.getDenuncia());
+            return "cidadao";
+        }
     }
 
     public String excluirDenuncia(int ID) {
@@ -113,5 +141,14 @@ public class DenunciaController {
 
     public void setDenuncia(Denuncia denuncia) {
         this.denuncia = denuncia;
+    }
+    
+    public String apoio(Denuncia d){
+        DenunciaDAO u = new DenunciaDAO();
+        this.denuncia = d;
+        this.denuncia.setNum_apoio(denuncia.getNum_apoio() +1);
+        u.deletarDenuncia(d.getId());
+        u.inserirDenuncia(this.denuncia);
+        return "listartodasdenuncias";
     }
 }
